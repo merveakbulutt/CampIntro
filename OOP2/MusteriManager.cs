4bd3c68fdbbc7609057cc9874724dfce414bf5aa
Katip <Katip@DESKTOP-6P2FP43>
2021-01-25 02:33:13 +0300

    Ders sonu

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{
+    class MusteriManager
+    {
+        //iş sınıfları-bussines classes
+        public void Ekle(Musteri musteri)
+        {
+
+            //if (musteri.)
+            //{
+
+            //}
+
+        }
+
+    }
+}
---
+++ OOP3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           IKrediManager ihtiyacKrediManager = new IhtiyacKrediManager();           
+           IKrediManager tasitKrediManager = new TasitKrediManager();
+           IKrediManager konutKrediManager = new KonutKrediManager();
+
+            ILoggerService datebaseLoggerService = new DataBaseLoggerService();
+            ILoggerService fileloggerservice = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(new EsnafKredisiManager(),new List<ILoggerService> {new DataBaseLoggerService(),new SmsLoggerService()});
+
+            List<IKrediManager> krediler = new List<IKrediManager> {ihtiyacKrediManager,tasitKrediManager};
+           // basvuruManager.KrediOnBilgilendirmesiYap(krediler);
+           
+        }
+    }
+}
